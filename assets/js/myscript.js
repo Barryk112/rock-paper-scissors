a28c723100183e3189db7c7acc4dd62f35e0f4a8
Barryk112 <barrykiely112@gmail.com>
2022-11-23 10:44:14 +0000

    Added js to the myscript.js file and checked if player choice is getting read in correctly

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,1 +1,18 @@
-console.log("Hello from js file");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "reset-score") {
+                restScore();
+            } else {
+                let playerChoice = this.getAttribute("data-type");
+                runGame(playerChoice);
+            }
+        });
+    }
+})
+
+function runGame(playerChoice) {
+    console.log(playerChoice);
+}
